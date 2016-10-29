dd6dc1546681a1857b1ad136b0f2150dbb77b863
Amanskywalker <ak47su30ac@gmail.com>
2016-10-29 13:45:26 +0530

    Added purchaseorderno src files  	new file:   src/purchaseorderno.cpp

---
+++ src/purchaseorderno.cpp
@@ -0,0 +1,16 @@
+#include "../include/purchaseorderno.h"
+
+using namespace std;
+
+	purchaseorderno()
+	{
+		ponumber=1000;
+	}
+	int getponumber()
+	{
+		return ponumber;
+	}
+	void setponumber()
+	{
+		ponumber++;
+	}
